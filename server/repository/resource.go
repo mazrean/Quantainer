2da7219748ebe9195e6a011ff6ea5b9e90aaaa64
mazrean <shunsuke.w04@gmail.com>
2021-12-04 18:01:26 +0900

    resourceリポジトリ抽象化

---
+++ server/repository/resource.go
@@ -0,0 +1,15 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/mazrean/Quantainer/domain"
+	"github.com/mazrean/Quantainer/domain/values"
+	"github.com/mazrean/Quantainer/service"
+)
+
+type Resource interface {
+	SaveResource(ctx context.Context, fileID values.FileID, resource *domain.Resource) error
+	GetResource(ctx context.Context, resourceID values.ResourceID) (*domain.Resource, error)
+	GetResources(ctx context.Context, resourceID values.ResourceID, params *service.ResourceSearchParams) ([]*domain.Resource, error)
+}

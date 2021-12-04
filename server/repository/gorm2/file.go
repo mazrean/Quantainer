210395c728d099d0fe15164c2c4ce38ae956f963
mazrean <shunsuke.w04@gmail.com>
2021-12-04 14:50:37 +0900

    SaveFile実装

--- server/repository/gorm2/file.go
+++ server/repository/gorm2/file.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/google/uuid"
+	"github.com/mazrean/Quantainer/domain"
+	"github.com/mazrean/Quantainer/domain/values"
 	"gorm.io/gorm"
 )
 
@@ -74,6 +77,56 @@ func setupFileTypeTable(db *gorm.DB) error {
 		if err != nil {
 			return fmt.Errorf("failed to create role type: %w", err)
 		}
+	}
+
+	return nil
+}
+
+func (f *File) SaveFile(ctx context.Context, file *domain.File) error {
+	db, err := f.db.getDB(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to get db: %w", err)
+	}
+
+	var fileTypeName string
+	switch file.GetType() {
+	case values.FileTypeJpeg:
+		fileTypeName = fileTypeJpeg
+	case values.FileTypePng:
+		fileTypeName = fileTypePng
+	case values.FileTypeWebP:
+		fileTypeName = fileTypeWebP
+	case values.FileTypeSvg:
+		fileTypeName = fileTypeSvg
+	case values.FileTypeGif:
+		fileTypeName = fileTypeGif
+	case values.FileTypeOther:
+		fileTypeName = fileTypeOther
+	default:
+		return fmt.Errorf("invalid file type: %d", file.GetType())
+	}
+
+	var fileType FileTypeTable
+	err = db.
+		Session(&gorm.Session{}).
+		Where("name = ?", fileTypeName).
+		Where("active").
+		Select("id").
+		Take(&fileType).Error
+	if err != nil {
+		return fmt.Errorf("failed to get file type: %w", err)
+	}
+	fileTypeID := fileType.ID
+
+	fileTable := FileTable{
+		ID: uuid.UUID(file.GetID()),
+		FileTypeID: fileTypeID,
+		CreatedAt: file.GetCreatedAt(),
+	}
+
+	err = db.Create(&fileTable).Error
+	if err != nil {
+		return fmt.Errorf("failed to create file: %w", err)
 	}
 
 	return nil

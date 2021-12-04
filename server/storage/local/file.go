08a277c5f458a671c5fc4718a74298d14863df28
mazrean <shunsuke.w04@gmail.com>
2021-12-04 15:02:54 +0900

    SaveFile実装

--- server/storage/local/file.go
+++ server/storage/local/file.go
@@ -1,6 +1,18 @@
 package local
 
-import "fmt"
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"io/fs"
+	"os"
+	"path"
+
+	"github.com/google/uuid"
+	"github.com/mazrean/Quantainer/domain"
+	"github.com/mazrean/Quantainer/storage"
+)
 
 type File struct {
 	fileRootPath     string
@@ -17,4 +29,29 @@ func NewFile(directoryManager *DirectoryManager) (*File, error) {
 		fileRootPath:     fileRootPath,
 		directoryManager: directoryManager,
 	}, nil
+}
+
+func (f *File) SaveFile(ctx context.Context, file *domain.File, reader io.Reader) error {
+	filePath := path.Join(f.fileRootPath, uuid.UUID(file.GetID()).String())
+
+	_, err := os.Stat(filePath)
+	if err == nil {
+		return storage.ErrAlreadyExists
+	}
+	if err != nil && !errors.Is(err, fs.ErrNotExist) {
+		return fmt.Errorf("failed to stat file: %w", err)
+	}
+
+	fl, err := os.Create(filePath)
+	if err != nil {
+		return fmt.Errorf("failed to open file: %w", err)
+	}
+	defer fl.Close()
+
+	_, err = io.Copy(fl, reader)
+	if err != nil {
+		return fmt.Errorf("failed to copy: %w", err)
+	}
+
+	return nil
 }

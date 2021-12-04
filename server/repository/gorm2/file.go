a1f8156652e0a6601865c3787d477e4ce28614ee
mazrean <shunsuke.w04@gmail.com>
2021-12-04 20:50:33 +0900

    GetFile実装

--- server/repository/gorm2/file.go
+++ server/repository/gorm2/file.go
@@ -2,11 +2,13 @@ package gorm2
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
 	"github.com/google/uuid"
 	"github.com/mazrean/Quantainer/domain"
 	"github.com/mazrean/Quantainer/domain/values"
+	"github.com/mazrean/Quantainer/repository"
 	"gorm.io/gorm"
 )
 
@@ -130,4 +132,54 @@ func (f *File) SaveFile(ctx context.Context, file *domain.File) error {
 	}
 
 	return nil
+}
+
+func (f *File) GetFile(ctx context.Context, fileID values.FileID, lockType repository.LockType) (*domain.File, error) {
+	db, err := f.db.getDB(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get db: %w", err)
+	}
+
+	db, err = f.db.setLock(db, lockType)
+	if err != nil {
+		return nil, fmt.Errorf("failed to set lock: %w", err)
+	}
+
+	var fileTable FileTable
+	err = db.
+		Session(&gorm.Session{}).
+		Joins("FileType").
+		Where("files.id = ?", fileID).
+		Select("files.id", "files.created_at", "file_types.name").
+		Take(&fileTable).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return nil, repository.ErrRecordNotFound
+	}
+	if err != nil {
+		return nil, fmt.Errorf("failed to get file type: %w", err)
+	}
+
+	var fileType values.FileType
+	switch fileTable.FileType.Name {
+	case fileTypeJpeg:
+		fileType = values.FileTypeJpeg
+	case fileTypePng:
+		fileType = values.FileTypePng
+	case fileTypeWebP:
+		fileType = values.FileTypeWebP
+	case fileTypeSvg:
+		fileType = values.FileTypeSvg
+	case fileTypeGif:
+		fileType = values.FileTypeGif
+	case fileTypeOther:
+		fileType = values.FileTypeOther
+	default:
+		return nil, fmt.Errorf("invalid file type: %s", fileTable.FileType.Name)
+	}
+
+	return domain.NewFile(
+		fileID,
+		fileType,
+		fileTable.CreatedAt,
+	), nil
 }

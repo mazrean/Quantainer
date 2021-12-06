30f93667ca8c32020bb1735d6f2279728e1f21d0
mazrean <shunsuke.w04@gmail.com>
2021-12-06 09:15:58 +0900

    GetResources実装

--- server/repository/gorm2/resource.go
+++ server/repository/gorm2/resource.go
@@ -150,3 +150,70 @@ func (r *Resource) GetResource(ctx context.Context, resourceID values.ResourceID
 
 	return &resource, nil
 }
+
+func (r *Resource) GetResources(ctx context.Context, params *repository.ResourceSearchParams) ([]*repository.ResourceWithCreator, error) {
+	db, err := r.db.getDB(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get db: %w", err)
+	}
+
+	resourceTypeNames := make([]string, 0, len(params.ResourceTypes))
+	for _, resourceType := range params.ResourceTypes {
+		switch resourceType {
+		case values.ResourceTypeImage:
+			resourceTypeNames = append(resourceTypeNames, resourceTypeImage)
+		case values.ResourceTypeOther:
+			resourceTypeNames = append(resourceTypeNames, resourceTypeOther)
+		default:
+			return nil, fmt.Errorf("invalid resource type: %d", resourceType)
+		}
+	}
+
+	creatorIDs := make([]uuid.UUID, 0, len(params.Users))
+	for _, creatorInfo := range params.Users {
+		creatorIDs = append(creatorIDs, uuid.UUID(creatorInfo.GetID()))
+	}
+
+	var resourceTables []ResourceTable
+	err = db.
+		Session(&gorm.Session{}).
+		Joins("ResourceType").
+		Joins("File").
+		Where("resource_types.name IN ?", resourceTypeNames).
+		Where("files.creator_id = ?", creatorIDs).
+		Limit(params.Limit).
+		Offset(params.Offset).
+		Select("resources.id", "resources.name", "resources.comment", "resources.created_at", "resource_types.name", "files.creator_id").
+		Find(&resourceTables).Error
+	if err != nil {
+		return nil, fmt.Errorf("failed to get resources: %w", err)
+	}
+
+	resources := make([]*repository.ResourceWithCreator, 0, len(resourceTables))
+	for _, resourceTable := range resourceTables {
+		var resourceType values.ResourceType
+		switch resourceTable.ResourceType.Name {
+		case resourceTypeImage:
+			resourceType = values.ResourceTypeImage
+		case resourceTypeOther:
+			resourceType = values.ResourceTypeOther
+		default:
+			return nil, fmt.Errorf("invalid resource type: %s", resourceTable.ResourceType.Name)
+		}
+
+		resource := repository.ResourceWithCreator{
+			Resource: domain.NewResource(
+				values.ResourceID(resourceTable.ID),
+				values.NewResourceName(resourceTable.Name),
+				resourceType,
+				values.NewResourceComment(resourceTable.Comment),
+				resourceTable.CreatedAt,
+			),
+			Creator: values.NewTrapMemberID(resourceTable.File.CreatorID),
+		}
+
+		resources = append(resources, &resource)
+	}
+
+	return resources, nil
+}

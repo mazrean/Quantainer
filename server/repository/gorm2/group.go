bd52ca067a03067f49d1a67be73adf55d35d9264
mazrean <shunsuke.w04@gmail.com>
2021-12-07 20:21:35 +0900

    SaveGroup実装

--- server/repository/gorm2/group.go
+++ server/repository/gorm2/group.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/google/uuid"
+	"github.com/mazrean/Quantainer/domain"
+	"github.com/mazrean/Quantainer/domain/values"
 	"gorm.io/gorm"
 )
 
@@ -124,6 +127,87 @@ func setupWritePermissionTypeTable(db *gorm.DB) error {
 		if err != nil {
 			return fmt.Errorf("failed to create read permission type: %w", err)
 		}
+	}
+
+	return nil
+}
+
+func (g *Group) SaveGroup(ctx context.Context, group *domain.Group, mainResource values.ResourceID) error {
+	db, err := g.db.getDB(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to get db: %w", err)
+	}
+
+	var groupTypeName string
+	switch group.GetType() {
+	case values.GroupTypeArtBook:
+		groupTypeName = groupTypeArtBook
+	case values.GroupTypeOther:
+		groupTypeName = groupTypeOther
+	default:
+		return fmt.Errorf("invalid group type: %d", group.GetType())
+	}
+
+	var groupType GroupTypeTable
+	err = db.
+		Session(&gorm.Session{}).
+		Where("name = ?", groupTypeName).
+		Take(&groupType).Error
+	if err != nil {
+		return fmt.Errorf("failed to get group type: %w", err)
+	}
+
+	var readPermissionName string
+	switch group.GetReadPermission() {
+	case values.GroupReadPermissionPublic:
+		readPermissionName = readPermissionPublic
+	case values.GroupReadPermissionPrivate:
+		readPermissionName = readPermissionPrivate
+	default:
+		return fmt.Errorf("invalid read permission: %d", group.GetReadPermission())
+	}
+
+	var readPermission ReadPermissionTable
+	err = db.
+		Session(&gorm.Session{}).
+		Where("name = ?", readPermissionName).
+		Take(&readPermission).Error
+	if err != nil {
+		return fmt.Errorf("failed to get read permission: %w", err)
+	}
+
+	var writePermissionName string
+	switch group.GetWritePermission() {
+	case values.GroupWritePermissionPublic:
+		writePermissionName = writePermissionPublic
+	case values.GroupWritePermissionPrivate:
+		writePermissionName = writePermissionPrivate
+	default:
+		return fmt.Errorf("invalid write permission: %d", group.GetWritePermission())
+	}
+
+	var writePermission WritePermissionTable
+	err = db.
+		Session(&gorm.Session{}).
+		Where("name = ?", writePermissionName).
+		Take(&writePermission).Error
+	if err != nil {
+		return fmt.Errorf("failed to get write permission: %w", err)
+	}
+
+	groupTable := GroupTable{
+		ID:                uuid.UUID(group.GetID()),
+		Name:              string(group.GetName()),
+		Description:       string(group.GetDescription()),
+		GroupTypeID:       groupType.ID,
+		MainResourceID:    uuid.UUID(mainResource),
+		ReadPermissionID:  readPermission.ID,
+		WritePermissionID: writePermission.ID,
+	}
+
+	err = db.Create(&groupTable).Error
+	if err != nil {
+		return fmt.Errorf("failed to create group: %w", err)
 	}
 
 	return nil

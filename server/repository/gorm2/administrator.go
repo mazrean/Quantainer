7c0a9bf171a4fd7863bd3f4f7311e642018e7bed
mazrean <shunsuke.w04@gmail.com>
2021-12-07 19:47:43 +0900

    SaveAdministrators実装

--- server/repository/gorm2/administrator.go
+++ server/repository/gorm2/administrator.go
@@ -1,5 +1,13 @@
 package gorm2
 
+import (
+	"context"
+	"fmt"
+
+	"github.com/google/uuid"
+	"github.com/mazrean/Quantainer/domain/values"
+)
+
 type Administrator struct {
 	db *DB
 }
@@ -8,4 +16,26 @@ func NewAdministrator(db *DB) *Administrator {
 	return &Administrator{
 		db: db,
 	}
+}
+
+func (a *Administrator) SaveAdministrators(ctx context.Context, groupID values.GroupID, administrators []values.TraPMemberID) error {
+	db, err := a.db.getDB(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to get db: %w", err)
+	}
+
+	administratorTables := make([]*AdministratorTable, 0, len(administrators))
+	for _, administrator := range administrators {
+		administratorTables = append(administratorTables, &AdministratorTable{
+			GroupID: uuid.UUID(groupID),
+			UserID:  uuid.UUID(administrator),
+		})
+	}
+
+	err = db.Create(&administratorTables).Error
+	if err != nil {
+		return fmt.Errorf("failed to save administrators: %w", err)
+	}
+
+	return nil
 }

738ec974bd1e1eaac98ba2ea3c83261bafb19307
mazrean <shunsuke.w04@gmail.com>
2021-12-08 18:22:19 +0900

    PatchGroup実装

--- server/handler/v1/group.go
+++ server/handler/v1/group.go
@@ -380,3 +380,135 @@ func (g *Group) GetGroup(c echo.Context, strGroupID Openapi.GroupIDInPath) error
 		},
 	})
 }
+
+func (g *Group) PatchGroup(c echo.Context, strGroupID Openapi.GroupIDInPath) error {
+	session, err := getSession(c)
+	if err != nil {
+		log.Printf("error: failed to get session: %v\n", err)
+		return echo.NewHTTPError(http.StatusInternalServerError, "failed to get session")
+	}
+
+	authSession, err := g.session.getAuthSession(session)
+	if err != nil {
+		log.Printf("error: failed to get auth session: %v\n", err)
+		return echo.NewHTTPError(http.StatusInternalServerError, "failed to get auth session")
+	}
+
+	uuidGroupID, err := uuid.Parse(string(strGroupID))
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "invalid group id")
+	}
+
+	var apiGroup Openapi.NewGroup
+	err = c.Bind(&apiGroup)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "invalid request body")
+	}
+
+	var groupType values.GroupType
+	switch apiGroup.Type {
+	case Openapi.GroupTypeArtBook:
+		groupType = values.GroupTypeArtBook
+	case Openapi.GroupTypeOther:
+		groupType = values.GroupTypeOther
+	default:
+		return echo.NewHTTPError(http.StatusBadRequest, "invalid group type")
+	}
+
+	var readPermission values.GroupReadPermission
+	switch apiGroup.ReadPermission {
+	case Openapi.ReadPermissionPublic:
+		readPermission = values.GroupReadPermissionPublic
+	case Openapi.ReadPermissionPrivate:
+		readPermission = values.GroupReadPermissionPrivate
+	default:
+		return echo.NewHTTPError(http.StatusBadRequest, "invalid read permission")
+	}
+
+	var writePermission values.GroupWritePermission
+	switch apiGroup.WritePermission {
+	case Openapi.WritePermissionPublic:
+		writePermission = values.GroupWritePermissionPublic
+	case Openapi.WritePermissionPrivate:
+		writePermission = values.GroupWritePermissionPrivate
+	default:
+		return echo.NewHTTPError(http.StatusBadRequest, "invalid write permission")
+	}
+
+	uuidResourceID, err := uuid.Parse(apiGroup.MainResourceID)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "invalid resource id")
+	}
+
+	resourceIDs := make([]values.ResourceID, 0, len(apiGroup.ResourceIDs))
+	for _, resourceID := range apiGroup.ResourceIDs {
+		uuidResourceID, err := uuid.Parse(resourceID)
+		if err != nil {
+			return echo.NewHTTPError(http.StatusBadRequest, "invalid resource id")
+		}
+		resourceIDs = append(resourceIDs, values.ResourceID(uuidResourceID))
+	}
+
+	groupDetail, err := g.groupServer.EditGroup(
+		c.Request().Context(),
+		authSession,
+		values.NewGroupIDFromUUID(uuidGroupID),
+		values.NewGroupName(apiGroup.Name),
+		groupType,
+		values.NewGroupDescription(apiGroup.Description),
+		readPermission,
+		writePermission,
+		values.NewResourceIDFromUUID(uuidResourceID),
+		resourceIDs,
+	)
+	if errors.Is(err, service.ErrNoGroup) {
+		return echo.NewHTTPError(http.StatusNotFound, "group not found")
+	}
+	if errors.Is(err, service.ErrInvalidPermission) {
+		return echo.NewHTTPError(http.StatusBadRequest, "invalid permission")
+	}
+	if errors.Is(err, service.ErrNoResource) {
+		return echo.NewHTTPError(http.StatusBadRequest, "no resource")
+	}
+	if errors.Is(err, service.ErrNoUser) {
+		return echo.NewHTTPError(http.StatusBadRequest, "no user")
+	}
+	if errors.Is(err, service.ErrForbidden) {
+		return echo.NewHTTPError(http.StatusForbidden, "forbidden")
+	}
+	if err != nil {
+		log.Printf("error: failed to edit group: %v\n", err)
+		return echo.NewHTTPError(http.StatusInternalServerError, "failed to edit group")
+	}
+
+	var resourceType Openapi.ResourceType
+	switch groupDetail.MainResource.Resource.GetType() {
+	case values.ResourceTypeImage:
+		resourceType = Openapi.ResourceTypeImage
+	case values.ResourceTypeOther:
+		resourceType = Openapi.ResourceTypeOther
+	default:
+		return echo.NewHTTPError(http.StatusInternalServerError, "invalid resource type")
+	}
+
+	administrators := make([]string, 0, len(groupDetail.Administers))
+	for _, administrator := range groupDetail.Administers {
+		administrators = append(administrators, string(administrator.GetName()))
+	}
+
+	return c.JSON(http.StatusOK, &Openapi.GroupDetail{
+		GroupBase:      apiGroup.GroupBase,
+		Administrators: administrators,
+		MainResource: Openapi.Resource{
+			Id:        uuid.UUID(groupDetail.MainResource.Resource.GetID()).String(),
+			FileID:    uuid.UUID(groupDetail.MainResource.File.GetID()).String(),
+			Creator:   string(groupDetail.MainResource.Creator.GetName()),
+			CreatedAt: groupDetail.GetCreatedAt(),
+			NewResource: Openapi.NewResource{
+				Name:         string(groupDetail.MainResource.GetName()),
+				Comment:      string(groupDetail.MainResource.GetComment()),
+				ResourceType: resourceType,
+			},
+		},
+	})
+}

5867c1bc2329fd3b1fb21c7f4c7cf0b96b6fe1d2
mazrean <shunsuke.w04@gmail.com>
2021-12-09 20:15:41 +0900

    subtitle

---
+++ client/src/components/SubTitleWithButton.svelte
@@ -0,0 +1,33 @@
+<script type="ts">
+  import { goto } from "$app/navigation";
+  import Button from "./Button.svelte";
+  import SubTitle from "./SubTitle.svelte";
+
+  export let title:string;
+  export let buttonLabel: string;
+  export let link:string;
+</script>
+
+<div class="container">
+  <SubTitle title={title} />
+  <span />
+  <div class="btn">
+    <Button label={buttonLabel} on:click={()=>goto(link)} />
+  </div>
+</div>
+
+<style>
+  .container {
+    display: inline-flex;
+    flex-direction: row;
+    align-items: flex-end;
+    padding: 0 16px;
+  }
+  span {
+    height: 100%;
+    width: 20px;
+  }
+  .btn {
+    margin: 5px 0;
+  }
+</style>
---
+++ client/src/routes/files/index.svelte
@@ -0,0 +1,7 @@
+<script type="ts">
+  import SubTitleWithButton from "../../components/SubTitleWithButton.svelte";
+</script>
+
+<div class="container">
+  <SubTitleWithButton title="Files" buttonLabel="New File" link="/files/new" />
+</div>
